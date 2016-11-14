f3bdcab74707a518a486858ccd5b8909ecaf308d
Jackie <123564542@qq.com>
2016-11-14 11:15:41 +0800

    submit.php edit

--- submit.php
+++ submit.php
@@ -1,14 +1,28 @@
 <?php
 $name=$_POST["name"];
 $address=$_POST["address"];
+$gender=$_POST["gender"];
+$email=$_POST["email"];
 
-if ($name !=null || $address!=null){
-    
-    echo "we got user's name is ".$name."<br/>";
-    echo "we got user's address is".$address;
+if ($name=null or $address=null or $mail=null or $gender='1'){
+    echo "All information need to be provided!";
+
 }else {
-    echo "Two column are both needed";
-    
+    if(!preg_match("/^[a-zA-Z]*$/", $name)){
+            echo "name is not valid";
+    }
+    if(preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){
+            echo "Please input an valid email";
+    }
+    echo "Name is ".$name."<br/>";
+    echo "Address is".$address."<br/>";
+    echo "Email is ".$email."<br/>";
+
+    if($gender='2'){
+    echo "Gender is Male";
+    }else{
+    echo "Gender is Female";
+    }
 }
 
 
